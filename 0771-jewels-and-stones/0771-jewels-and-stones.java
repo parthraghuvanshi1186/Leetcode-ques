cf228b1a99c66d4285cbd393412013808603c87a
Parth singh <parthraghuvanshi0001@gmail.com>
2026-08-17 15:49:51 +0530

    Time: 0 ms (100%), Space: 43.2 MB (37.04%) - LeetHub

--- 0771-jewels-and-stones/0771-jewels-and-stones.java
+++ 0771-jewels-and-stones/0771-jewels-and-stones.java
@@ -1,15 +1,12 @@
 class Solution {
     public int numJewelsInStones(String j, String s) {
         int[] freq=new int[256];
-        int[] freq2=new int[256];
-        for(int i=0;i<j.length();i++){
-           freq[j.charAt(i)]++;
-        }for(int i=0;i<s.length();i++){
-            freq2[s.charAt(i)]++;
+        for(int i=0;i<s.length();i++){
+           freq[s.charAt(i)]++;
         }int count=0;
-        for(int i=0;i<256;i++){
-            if(freq[i]>0 && freq2[i]>0){
-                count+=freq2[i];
+        for(int i=0;i<j.length();i++){
+            if(freq[j.charAt(i)]>0){
+                count+=freq[j.charAt(i)];
             }
         }return count;
     }
