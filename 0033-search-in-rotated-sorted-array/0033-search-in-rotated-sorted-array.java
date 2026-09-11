effcae242577efc26e07cba47b7f0f3c16e48fc8
Parth singh <parthraghuvanshi0001@gmail.com>
2026-09-12 01:41:56 +0530

    Time: 0 ms (100%), Space: 43.7 MB (82.26%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,30 +1,23 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int left=0,right=nums.length-1;
-        while(left<right){
-            int mid=left+(right-left)/2;
-            if(nums[mid]>nums[right]){
-                left=mid+1;
-            }else{
-                right=mid;
-            }
-        }
-        int pivot=right;
-        int ans=search(nums,target,0,pivot-1);
-        if(ans>=0) return ans;
-        return search(nums,target,pivot,nums.length-1); 
-    }
-    public int search(int[] nums,int target,int left,int right){
+        int left=0;int right=nums.length-1;
         while(left<=right){
             int mid=left+(right-left)/2;
             if(nums[mid]==target){
                 return mid;
-            }else if(nums[mid]>target){
-                right=mid-1;
+            }if(nums[left]<=nums[mid]){
+                if(target>=nums[left] && target<nums[mid]){
+                    right=mid-1;
+                }else{
+                    left=mid+1;
+                }
             }else{
-                left=mid+1;
+                if(target>nums[mid]&&target<=nums[right]){
+                    left=mid+1;
+                }else{
+                    right=mid-1;
+                }
             }
-        }
-        return -1;
+        }return -1;
     }
 }
