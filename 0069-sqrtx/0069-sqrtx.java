ee648c8b6ba9cfdc992c763a6c6503f534a321ee
Parth singh <parthraghuvanshi0001@gmail.com>
2026-09-12 01:56:30 +0530

    Time: 1 ms (99.08%), Space: 42.8 MB (23.62%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -4,12 +4,12 @@ class Solution {
         int left=1;int right=x;int ans =0;
         while(left<=right){
             int mid=left+(right-left)/2;
-            if(mid<=x/mid){
+            if((long)mid*mid<=x){
                 ans=mid;
-                left=mid+1;
+              left=mid+1;
             }
             else{
-                right=mid-1;
+                 right=mid-1;
             }
         }return ans;
     }
